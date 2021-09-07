04f4e350228d4ef4f274d1c5961a2d4d9277d216
Fabio Frasson <fabio.frass@gmail.com>
2021-09-07 17:24:48 -0300

    JUnit lifecycle demo

--- src/test/java/guru/springframework/GreetingTest.java
+++ src/test/java/guru/springframework/GreetingTest.java
@@ -1,14 +1,19 @@
 package guru.springframework;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 class GreetingTest {
 
   private Greeting greeting;
 
+  @BeforeAll
+  public static void beforeClass() {
+    System.out.println("Before all...");
+  }
+
   @BeforeEach
   void setUp() {
+    System.out.println("Before each...");
     greeting = new Greeting();
   }
 
@@ -20,5 +25,15 @@ class GreetingTest {
   @Test
   void testHelloWorld() {
     System.out.println(greeting.helloWorld("Fabio"));
+  }
+
+  @AfterEach
+  void tearDown() {
+    System.out.println("After each...");
+  }
+
+  @AfterAll
+  public static void afterClass() {
+    System.out.println("After all...");
   }
 }
